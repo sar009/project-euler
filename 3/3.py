3b0578150aa8f6143d05033a262dd96a4ebc36d7
sarad mohanan <sarad.mohanan@gmail.com>
2014-05-31 09:24:45 +0530

    added questions

--- 3/3.py
+++ 3/3.py
@@ -1,12 +1,24 @@
-num=0
-for i in range(2, 600851475143+1, 1):
-	if(600851475143%i==0):
-		k=int(600851475143/i)
-		for j in range(2, k+1, 1):
-			if (k%j==0):
-				if (k==j):
+#
+# The prime factors of 13195 are 5, 7, 13 and 29.
+#
+# What is the largest prime factor of the number 600851475143 ?
+#
+# Answer: 6857
+#
+
+num = 0
+i = 0
+while i < 600851475143:
+	i += 1
+	if (600851475143%i == 0):
+		k = int(600851475143/i)
+		j = 0
+		while j < k+1:
+			j += 1
+			if k%j == 0:
+				if k == j:
 					num=k
 				break
-	if (num>0):
+	if num > 0:
 		break
-print(num)
+print num
